5d7ea32ee1ae782a8c82c6ab775caebbd4d07c29
PPG <reply2ppg@yahoo.com>
2018-04-30 23:17:03 -0400

    Add swagger support.

--- src/main/java/com/ppg/services/movieprice/config/AppConfig.java
+++ src/main/java/com/ppg/services/movieprice/config/AppConfig.java
@@ -1,18 +1,34 @@
 package com.ppg.services.movieprice.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.core.env.Environment;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.resource.CssLinkResourceTransformer;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebMvc
 public class AppConfig  implements WebMvcConfigurer {
 
+    @Autowired
+    Environment environment;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedOrigins("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
--- src/main/java/com/ppg/services/movieprice/config/SecurityConfig.java
+++ src/main/java/com/ppg/services/movieprice/config/SecurityConfig.java
@@ -21,6 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and()
         .authorizeRequests()
-                .antMatchers("*").permitAll();
+                .antMatchers("*",
+                        "/swagger-ui.html**")
+                .permitAll();
     }
 }
